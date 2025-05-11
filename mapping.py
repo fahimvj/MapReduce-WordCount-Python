5233f3125a4868b900aeee12786948a713a5fdb6
MD Fahim Shahriar Chowdhury <5455584+fahimvj@users.noreply.github.com>
2025-05-11 17:14:00 +0200

    Update mapping.py

--- mapping.py
+++ mapping.py
@@ -1,12 +1,11 @@
-def mapper(chunk_file):
+def mapper(key, value):
     word_counts = []
-    with open(chunk_file, 'r', encoding='utf-8') as f:
-        for line in f:
-            words = line.split()
-            for word in words:
-                word_counts.append((word, 1))
+    words = value.split()
+    for word in words:
+        word_counts.append((word, 1))
     return word_counts
 
 if __name__ == '__main__':
-    chunk_file = 'chunk_0.txt'
-    print(mapper(chunk_file))
+    key = 0  
+    value = "hello world hello mapreduce"  # Example value (line content)
+    print(mapper(key, value))
